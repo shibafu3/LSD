b4c978264fa1d2b529f2d28a2fe7ba39b817931c
shibafu3 <shibafu3@shibafu3.com>
2017-08-03 20:34:17 +0900

    first commit

---
+++ hough/main.cpp
@@ -0,0 +1,174 @@
+#ifdef _DEBUG
+//Debugモードの場合
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_aruco310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_bgsegm310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_calib3d310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_ccalib310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_core310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_datasets310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_dnn310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_dpm310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_face310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_features2d310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_flann310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_fuzzy310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_highgui310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_imgcodecs310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_imgproc310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_line_descriptor310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_ml310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_objdetect310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_optflow310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_photo310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_plot310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_reg310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_rgbd310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_saliency310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_shape310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_stereo310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_stitching310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_structured_light310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_superres310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_surface_matching310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_text310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_tracking310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_ts310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_video310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_videoio310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_videostab310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_xfeatures2d310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_ximgproc310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_xobjdetect310d.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_xphoto310d.lib") // opencv_core
+#else											  
+//Releaseモードの場合								
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_aruco310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_bgsegm310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_calib3d310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_ccalib310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_core310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_datasets310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_dnn310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_dpm310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_face310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_features2d310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_flann310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_fuzzy310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_highgui310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_imgcodecs310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_imgproc310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_line_descriptor310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_ml310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_objdetect310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_optflow310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_photo310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_plot310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_reg310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_rgbd310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_saliency310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_shape310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_stereo310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_stitching310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_structured_light310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_superres310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_surface_matching310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_text310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_tracking310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_ts310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_video310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_videoio310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_videostab310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_xfeatures2d310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_ximgproc310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_xobjdetect310.lib")
+#pragma comment(lib,"C:\\opencv310\\build\\install\\x86\\vc12\\lib\\opencv_xphoto310.lib")
+#endif
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <vector>
+#include <list>
+#include "lsd.h"
+
+
+
+cv::Mat img;
+int n_lines;
+double* lines;
+
+void change_th_lsd(int nfa, void* dummy)
+{
+	cv::Mat result = img.clone();
+	for (int i = 0; i < n_lines; i++)
+	{
+		const double *line = &lines[i * 7];
+		if (nfa < line[6])
+		{
+			const cv::Point p1(line[0], line[1]);
+			const cv::Point p2(line[2], line[3]);
+			cv::line(result, p1, p2, cv::Scalar(0, 0, 255));
+		}
+	}
+	cv::imshow("result_image", result);
+}
+
+int main(int argc, char *argv[])
+{
+
+	cv::VideoCapture cap(0);
+	while (1){
+		cap >> img;
+		cv::cvtColor(img, img, CV_RGB2GRAY);
+		cv::resize(img, img, cv::Size(0, 0), 0.2, 0.2);
+
+
+		////画像をグレースケールとして読み込み
+		//img = cv::imread(argv[1], 0);
+
+		//LSD用画像に変換＞＜
+		double *dat = new double[img.rows * img.cols];
+		for (int y = 0; y < img.rows; y++)
+			for (int x = 0; x < img.cols; x++)
+				dat[y * img.cols + x] = img.at<unsigned char>(y, x);
+
+		//LSD処理
+		lines = lsd(&n_lines, dat, img.cols, img.rows);
+
+		//しきい値の最大値と最小値をもってくる
+		int max_NFA = 0;
+		for (int i = 0; i < n_lines; i++)
+			max_NFA = std::max(max_NFA, static_cast<int>(lines[i * 7 + 6]));
+
+		//結果描画用画像
+		cv::cvtColor(img, img, CV_GRAY2RGB);
+
+		//結果表示用ウィンドウ
+		//cv::namedWindow("result_image");
+		//cv::createTrackbar("NFA", "result_image", NULL, max_NFA, change_th_lsd);
+		//cv::setTrackbarPos("NFA", "result_image", max_NFA);
+
+
+		cv::Mat result = img.clone();
+		for (int i = 0; i < n_lines; i++)
+		{
+			const double *line = &lines[i * 7];
+			if (0 < line[6])
+			{
+				const cv::Point p1(line[0], line[1]);
+				const cv::Point p2(line[2], line[3]);
+				cv::line(result, p1, p2, cv::Scalar(0, 0, 255));
+			}
+		}
+
+
+		//結果表示
+		cv::imshow("result_image", result);
+		cv::waitKey(1);
+
+	}
+}
